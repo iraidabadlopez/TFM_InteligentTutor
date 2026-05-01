4e0856f6cc5c0f09fb2e8c894372b1d40ba02864
Iraida A. <iabad020@ikasle.ehu.eus>
2026-05-01 13:53:12 +0200

    track .keras and .pkl with lfs

--- modelos/dependencies.bat
+++ modelos/dependencies.bat
@@ -1,1 +1,1 @@
-python -m pip install matplotlib numpy scikit-image scikit-learn pandas tensorflow ipynb
+python -m pip install matplotlib numpy scikit-image scikit-learn pandas tensorflow ipynb torch
