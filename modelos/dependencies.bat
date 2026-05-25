eff10d4310754b4e7a449cc86ef50152140036bb
iraidabadlopez <iraidabadlopez@gmail.com>
2026-05-25 16:01:54 +0200

    update dependencies for python 3.9

--- modelos/dependencies.bat
+++ modelos/dependencies.bat
@@ -1,1 +1,1 @@
-python -m pip install matplotlib numpy scikit-image scikit-learn pandas tensorflow ipynb torch
+python -m pip install matplotlib numpy<2 scikit-image scikit-learn pandas tensorflow==2.10.0 torch seaborn
